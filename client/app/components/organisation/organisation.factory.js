4ac4422d81650ad0023c6c379f8ef3ffde08a3f2
Sean Stanley <sean@foodcoop.org.nz>
2017-01-13 15:31:28 +1300

    api typo fix

--- client/app/components/organisation/organisation.factory.js
+++ client/app/components/organisation/organisation.factory.js
@@ -1,7 +1,7 @@
 const OrganisationFactory = ($http) => {
   'ngInject';
 
-  const getOrganisationData = (id, options = {}) => $http.get(`/api/one/organisations/${id}`, options).then((response) => response.data);
+  const getOrganisationData = (id, options = {}) => $http.get(`/api/organisations/one/${id}`, options).then((response) => response.data);
   const getBookmarksData = (id, options = {}) => $http.get('/api/organisations/bookmarks', options).then((response) => response.data);
   const getNewestData = (id, options = {}) => $http.get('/api/organisations/newest', options).then((response) => response.data);
   const getPopularData = (id, options = {}) => $http.get('/api/organisations/popular', options).then((response) => response.data);
