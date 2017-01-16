a339dd06c4a7edf831c1fb902621c6a8efd13d82
Sean Stanley <sean@foodcoop.org.nz>
2017-01-16 16:37:04 +1300

    organisation team member permissions

--- client/app/common/organisationEditTeamMember/organisationEditTeamMember.controller.js
+++ client/app/common/organisationEditTeamMember/organisationEditTeamMember.controller.js
@@ -13,13 +13,13 @@ class OrganisationEditTeamMemberController {
     return Math.random();
   }
 
-  modify() {
+  modify(value) {
     const stateChange = 'team';
     if (this.isNew) {
       const modifyDetails = { type: 'insert', field: 'team', value: { _id: this.getId(), name: this.teamMember.name, jobTitle: this.teamMember.jobTitle, emails: this.teamMember.emails } };
       this.modifyTeamMember({ modifyDetails, stateChange });
     } else {
-      const modifyDetails = { type: 'update', field: 'team', index: this.index, value: { _id: this.teamMember._id, name: this.teamMember.name, jobTitle: this.teamMember.jobTitle, emails: this.teamMember.emails } };
+      const modifyDetails = { type: 'update', field: 'team', index: this.index, value };
       this.modifyTeamMember({ modifyDetails, stateChange });
     }
   }
