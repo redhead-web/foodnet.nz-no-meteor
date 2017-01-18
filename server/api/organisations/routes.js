67bf3bbc53dd1724508491fd4d99a82c78eba19d
Sean Stanley <sean@foodcoop.org.nz>
2017-01-18 13:01:20 +1300

    move fixture data to it's own file

--- server/api/organisations/routes.js
+++ server/api/organisations/routes.js
@@ -1,6 +1,7 @@
 /* eslint-disable new-cap */
 const express = require('express');
 const router = express.Router();
+const organisationsFixture = require('../db/organisations');
 
 router.get('/newest', (req, res) => {
   res.json({ organisations: [
@@ -39,31 +40,7 @@ router.get('/bookmarks', (req, res) => {
 });
 
 router.get('/one/:organisationId', (req, res) => {
-  res.json({
-    organisation: {
-      _id: 'someId',
-      name: 'Whangarei Food Co-op',
-      emails: [{ address: 'email@email.com', primaryContact: 'true' }],
-      banner: 'http://lorempixel.com/400/200/nature',
-      tags: 'carrots',
-      tagLine: 'Local - Fresh - Affordable',
-      description: { short: 'spoon hammer fork', long: 'spoon hammer fork mustard cheese' },
-      openingHours: 'Tuesdays 3-6pm',
-      type: 'Co-operitive Company',
-      registration: 'Company Registration Number',
-      gstReg: false,
-      gstNum: 'GST Number',
-      website: 'https://foodcoop.nz',
-      social: { facebook: 'facebook link', twitter: 'twitter link', instagram: 'instagram link', youtube: 'youtube link' },
-    },
-    locations: [{ address: '1 woods road', headOffice: true }, { address: 'some place else' }],
-    inputs: [{ _id: 'someId', name: 'carots', provider: 'Northland Naturals' }, { _id: 'someOtherId', name: 'beatroot', provider: 'Northland Naturals' }],
-    outputs: [{ _id: 'someId', name: 'carots' }, { _id: 'someOtherId', name: 'beatroot' }],
-    team: [
-      { _id: 'someId', name: 'Sean Stanley', jobTitle: 'some title', emails: [{ address: 'hello@hello.com' }] },
-      { _id: 'someId', name: 'Matt Stanley', jobTitle: 'some title', emails: [{ address: 'hello@hello.com' }] },
-      { _id: 'someId', name: 'Clive McKeg', jobTitle: 'some title', emails: [{ address: 'hello@hello.com' }] }],
-  });
+  res.json(organisationsFixture[0]);
 });
 
 module.exports = router;
