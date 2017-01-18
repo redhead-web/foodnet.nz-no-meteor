340ab5546fe3fe348a818deba4626f940de94383
Sean Stanley <sean@foodcoop.org.nz>
2017-01-19 12:09:40 +1300

    load organisations from neo4j

--- server/api/organisations/routes.js
+++ server/api/organisations/routes.js
@@ -1,15 +1,44 @@
-/* eslint-disable new-cap */
+/* eslint-disable new-cap, prefer-arrow-callback, no-plusplus, no-underscore-dangle */
 const express = require('express');
 const router = express.Router();
 const organisationsFixture = require('../db/organisations');
 
-router.get('/newest', (req, res) => {
-  res.json({ organisations: [
-    { _id: 'someId', name: 'Super Awesome Company', tags: ['carrots'] },
-    { _id: 'someOtherId', name: 'A Company', tags: ['beans'] },
-    { _id: 'someMoreId', name: 'Another Company', tags: ['HTML'] },
-    { _id: 'someAlternateId', name: 'Yet Another Company', tags: ['Lamb'] },
-  ] });
+const driver = require('../neo4j');
+
+function toCollection(records) {
+  const result = [];
+  for (let i = 0; i < records.length; i++) {
+    result.push(toObject(records[i]));
+  }
+
+  return result;
+}
+
+function toObject(record) {
+  const object = {};
+  for (let i = 0; i < record.keys.length; i++) {
+    object[record.keys[i]] = record._fields[i];
+  }
+  return object;
+}
+
+router.get('/newest', (req, res, next) => {
+  const session = driver.session();
+  const query = 'MATCH (n:Organisation) RETURN n.name AS name, n._id AS _id LIMIT 10';
+  session.run(query).then((result) => {
+    const data = { organisations: toCollection(result.records) };
+    res.json(data);
+    session.close();
+  }, (error) => {
+    next(error);
+  });
+
+  // res.json({ organisations: [
+  //   { _id: 'someId', name: 'Super Awesome Company', tags: ['carrots'] },
+  //   { _id: 'someOtherId', name: 'A Company', tags: ['beans'] },
+  //   { _id: 'someMoreId', name: 'Another Company', tags: ['HTML'] },
+  //   { _id: 'someAlternateId', name: 'Yet Another Company', tags: ['Lamb'] },
+  // ] });
 });
 
 router.get('/popular', (req, res) => {
