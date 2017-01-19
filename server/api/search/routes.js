4196e2ee9781cb1aaa7873078032aa0d89ec7f89
Alex-Mason-1982 <alexander.j.f.mason@gmail.com>
2017-01-19 15:21:32 +1300

    search

--- server/api/search/routes.js
+++ server/api/search/routes.js
@@ -1,30 +1,8 @@
 /* eslint-disable new-cap */
 const express = require('express');
 const router = express.Router();
-// const filter = require('lodash').filter;
 const driver = require('../neo4j');
 const utils = require('../db/utils');
-
-/*
-const testOrganisations = [
-  {
-    _id: 'someId',
-    name: 'Whangarei Food Co-op',
-    tags: 'carrots',
-    banner: 'http://lorempixel.com/400/200/business',
-    inputsCount: 5,
-    outputsCount: 400,
-  },
-  {
-    _id: 'someId2',
-    name: 'Whangarei Growers Market',
-    tags: 'celery',
-    banner: 'http://lorempixel.com/400/200/nature',
-    inputsCount: 10,
-    outputsCount: 1000,
-  },
-];
-*/
 
 router.get('/common', (req, res) => {
   res.json(['Carrots', 'Organic Fertilizer', 'Lamb Mince', 'Tomatoes', 'Websites', 'Packaging', 'HTML', 'Lettuce', 'Organic Sweeteners']);
